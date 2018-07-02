7b7ef96a31ac2e537bcd53b32e310f71dddfc646
songjunke <songjunke@baidu.com>
2018-07-02 18:15:21 +0800

    deploy

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+gitbook build
+
+# 进入生成的文件夹
+cd ./_book
+
+# 把生成的内容发布到github gh-pages
+git init
+git add -A
+git commit -m 'deploy'
+
+
+# 发布到git@github.com:hmtongji/chrome-plugin.git master:gh-pages
+git push -f https://github.com/hmtongji/chrome-plugin.git master:gh-pages
+
+cd -
